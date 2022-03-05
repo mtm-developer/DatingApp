d372b3dc7bef0222acf51cc11b0e42b96e335c4d
Michael McLeod <mtmcleod.developer@gmail.com>
2022-03-05 13:34:42 -0500

    End of Section 7

--- API/Startup.cs
+++ API/Startup.cs
@@ -57,7 +57,7 @@ namespace API
             //    app.UseSwagger();
             //    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             //}
-            //app.UseMiddleware<ExceptionMiddleware>();
+            app.UseMiddleware<ExceptionMiddleware>();
 
             app.UseHttpsRedirection();
 
