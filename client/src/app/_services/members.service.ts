69aa4db04c76ce8ba31365aac82c5e2fae6e6d89
Michael McLeod <mtmcleod.developer@gmail.com>
2022-03-14 19:31:16 -0400

    End of Section 11

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -26,7 +26,7 @@ export class MembersService {
   }
 
   getMember(username: string) {
-    const member = this.members.find(x => x.username = username);
+    const member = this.members.find(x => x.username === username);
     if (member !== undefined) return of(member);
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
@@ -38,6 +38,13 @@ export class MembersService {
         this.members[index] = member;
       })
     );
+  }
 
+  setMainPhoto(photoId: number) {
+    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photoId, {});
+  }
+
+  deletePhoto(photoId: number) {
+    return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
   }
 }
